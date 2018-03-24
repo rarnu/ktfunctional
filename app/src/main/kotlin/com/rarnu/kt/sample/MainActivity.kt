c546a3058a795936bd54c04ae6d17dad1d372a91
何晓杰 <hexiaojie@hujiang.com>
2018-03-24 12:51:19 +0800

    add: some code

---
+++ app/src/main/kotlin/com/rarnu/kt/sample/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.rarnu.kt.sample
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+    private fun testZip() {
+    }
+}
